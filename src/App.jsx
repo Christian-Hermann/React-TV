9a5a331fa901167d921bbcfbefd3277708b74c82
Your NameChristian <christianghermann@gmail.com>
2025-05-13 16:14:24 -0500

    finished homework

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,23 @@
  * React TV is an web streaming platform that allows users to browse
  * through the episodes of a variety of different shows.
  */
+
+import { useState } from "react";
+import ShowDetails from "./shows/ShowDetails";
+import ShowSelection from "./shows/ShowSelection";
+import { tvShows } from "./shows/data";
+
 export default function App() {
+  const [selectedShow, setSelectedShow] = useState(null);
   return (
     <>
       <header>
         <p>React TV</p>
+        <ShowSelection shows={tvShows} setSelectedShow={setSelectedShow} />
       </header>
-      <main></main>
+      <main>
+        <ShowDetails show={selectedShow} />
+      </main>
     </>
   );
 }
